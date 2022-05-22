ee56c3e6d98d28150ac674a6aac9f1971fed37b3
Shnnly <gprimila@gmail.com>
2022-05-21 21:34:19 -0300

    Teste JavaScript-2 2 :D

--- Pratica-1.js
+++ Pratica-1.js
@@ -1,11 +1,25 @@
-function legalAgeOrNot (age) {
-    // escreva aqui a solução de código que verifica se a idade da pessoa é maior ou não que 18 anos (veja que a idade virá através da variável 'age' que está como parâmetro da função)
-    // Se for maior ou igual, você deve exibir na tela "A idade é maior ou igual a 18 anos. Pode entrar na boate".
-    // Se for menor, você deve exibir na tela "A idade é menor que 18 anos, entrada não permitida!"
-}
-
 let age1 = 13
 let age2 = 18
 let age3 = 20
 
-// chame a função legalAgeOrNot que você construiu logo abaixo deste comentário para cada uma das idades em age1, age2, age3.
+function LegalOrNot () {
+    if (age1 == 18) {
+        console.log("A idade é maior ou igual a 18 anos. Pode entrar na boate");
+    } else{
+        console.log("A idade é menor que 18 anos, entrada não permitida!");
+    }
+
+    if (age2 == 18) {
+        console.log("A idade é maior ou igual a 18 anos. Pode entrar na boate");
+    } else{
+        console.log("A idade é menor que 18 anos, entrada não permitida!");
+    }
+
+    if (age3 == 18) {
+        console.log("A idade é maior ou igual a 18 anos. Pode entrar na boate");
+    } else{
+        console.log("A idade é menor que 18 anos, entrada não permitida!");
+    }
+}
+
+LegalOrNot()
---
+++ Pratica-2.js
@@ -0,0 +1,4 @@
+for (let contador = 1; contador <= 10; contador++) {
+    // escreva aqui um código para calcular exibir o resultado da tabuada do número 7. Ou seja, exibir o resultado de quanto é 7x1, 7x2, 7x3, 7x4, 7x5, etc
+    // veja que você pode utilizar o contador para fazer este cálculo.
+}
